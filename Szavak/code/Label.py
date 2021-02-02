3d54bc1cd88a428464618c5726817e81baccb524
Jannik <jannik.haberbosch@gmail.com>
2021-02-02 01:50:42 +0100

    Improvements.

--- Szavak/code/Label.py
+++ Szavak/code/Label.py
@@ -14,7 +14,7 @@ class Label(EventSprite):
 		self.config = _config
 		
 		self.text = self.config["text"] if "text" in self.config else "<example-text>"
-		self.font_name = self.config["font_name"] if "font_name" in self.config else ""
+		self.font_name = self.config["font_name"] if "font_name" in self.config else "Lucida Console"
 		self.font_size = self.config["font_size"] if "font_size" in self.config else 30
 		self.anti_aliasing = self.config["anti_aliasing"] if "anti_aliasing" in self.config else True
 		self.fg_colour = self.config["fg_colour"] if "fg_colour" in self.config else (0, 0, 0)
@@ -38,6 +38,30 @@ class Label(EventSprite):
 		self.rect.left = self.left
 	
 	
+	def set_text(
+		self,
+		_text):
+		
+		self.text = _text	
+	
+	
+	def render(
+		self):
+
+		self.image = self.font.render(
+			self.text,
+			self.anti_aliasing,
+			self.fg_colour,
+			self.bg_colour)
+			
+		old_top = self.rect.top
+		old_left = self.rect.left
+		self.rect = self.image.get_rect()
+		
+		self.rect.top = old_top
+		self.rect.left = old_left
+
+		
 	def set_background(self, _colour):
 		self.image = self.font.render(
 			self.text,
